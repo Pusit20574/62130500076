edc6dd83eba07b6894c58fcc214721f9d0d69b67
Pusit20574 <pusit.20574@mail.kmutt.ac.th>
2021-02-03 13:01:39 +0700

    [Add] groupwork2

---
+++ 62130500076_groupwork2/Profile.js
@@ -0,0 +1,16 @@
+const app = {
+    data() {
+        return {
+            name: 'Pusit Assawateerakeat',
+            position: 'SIT Student',
+            img: './images/phu.png',
+            article: 82,
+            followers: '9 M',
+            rating: 10,
+            facebook: 'https://www.facebook.com/Pusit.44/',
+            ig: 'https://www.instagram.com/phu_pusit/'
+        }
+    }
+}
+
+Vue.createApp(app).mount('#app')
